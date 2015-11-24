b6176521a9903a9059b81d74a503294372da119e
literat <tomas@litera.me>
2015-11-24 22:31:38 +0100

    Display chronosly events between posts

--- functions.php
+++ functions.php
@@ -1061,6 +1061,6 @@ function enlightenment_filter_credit_links_args( $args ) {
 add_filter( 'pre_get_posts', 'my_get_posts' );
 function my_get_posts( $query ) {
 	if ( is_home() && false == $query->query_vars['suppress_filters'] )
-		$query->set( 'post_type', array( 'post', 'event' ) );
+		$query->set( 'post_type', array( 'post', 'chronosly' ) );
 	return $query;
 }
